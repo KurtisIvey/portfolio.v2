14e2c7853e1901e4c049d448a358e1359c02a28f
Kitsunebackfire <kwolz92@gmail.com>
2023-02-03 10:39:47 -0600

    add custom duration times to skillLogo.js object, add specific value on logo motion.div to x:-75 because any higher caused some logos to not show on small res

--- src/assets/skillLogos.js
+++ src/assets/skillLogos.js
@@ -10,16 +10,66 @@ import reduxLogo from "./reduxLogo.svg";
 import tailwindLogo from "./tailwind.svg";
 
 const logos = [
-  { url: cssLogo },
-  { url: firebaseLogo },
-  { url: gitLogo },
-  { url: htmlLogo },
-  { url: jsLogo },
-  { url: mongoLogo },
-  { url: nodejsLogo },
-  { url: reactLogo },
-  { url: reduxLogo },
-  { url: tailwindLogo },
+  {
+    url: cssLogo,
+    name: "CSS3",
+    id: "1",
+    duration: 1,
+  },
+  {
+    url: firebaseLogo,
+    name: "Firebase",
+    id: "2",
+    duration: 1.1,
+  },
+  {
+    url: gitLogo,
+    name: "Git",
+    id: "3",
+    duration: 1.2,
+  },
+  {
+    url: htmlLogo,
+    name: "Html5",
+    id: "4",
+    duration: 1.3,
+  },
+  {
+    url: jsLogo,
+    name: "Javascript",
+    id: "5",
+    duration: 1.4,
+  },
+  {
+    url: mongoLogo,
+    name: "MongoDB",
+    id: "6",
+    duration: 1.5,
+  },
+  {
+    url: nodejsLogo,
+    name: "Node.js",
+    id: "7",
+    duration: 1.6,
+  },
+  {
+    url: reactLogo,
+    name: "React",
+    id: "8",
+    duration: 1.7,
+  },
+  {
+    url: reduxLogo,
+    name: "Redux",
+    id: "9",
+    duration: 1.8,
+  },
+  {
+    url: tailwindLogo,
+    name: "Tailwind",
+    id: "10",
+    duration: 1.9,
+  },
 ];
 
 export default logos;
--- src/sections/Skills.jsx
+++ src/sections/Skills.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import logos from "../assets/skillLogos";
+import { motion } from "framer-motion";
 
 const Skills = () => {
   return (
@@ -7,9 +8,23 @@ const Skills = () => {
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         Skills
       </h3>
-      <div className="grid grid-cols-4 gap-5">
+      <div className="grid grid-cols-3 gap-5 mt-8">
         {logos.map((logo) => (
-          <img src={logo.url} alt="" />
+          <motion.div
+            initial={{ x: -75, opacity: 0, scale: 0.5 }}
+            transition={{ duration: logo.duration }}
+            whileInView={{ x: 0, opacity: 1, scale: 1 }}
+            viewport={{ once: true }}
+            key={logo.id}
+            className=" flex flex-col justify-center items-center gap-3 mx-3"
+          >
+            <img
+              src={logo.url}
+              alt={logo.name}
+              className="object-cover rounded-sm w-auto h-10 sm:h-14"
+            />
+            <p className="text-center">{logo.name}</p>
+          </motion.div>
         ))}
       </div>
     </div>
