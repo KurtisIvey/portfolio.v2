e52d44afb0385a9a919d31d275581f2fb7028d01
Kitsunebackfire <kwolz92@gmail.com>
2023-02-08 15:54:35 -0600

    center project h3

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -12,7 +12,7 @@ const Projects = () => {
       className="relative h-full  flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly 
     mx-auto items-center z-0 mt-[11%]"
     >
-      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl  ml-5">
         Projects
       </h3>
       <div
