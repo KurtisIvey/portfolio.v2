cf2196bc5f6bc8ab7f79ad03d7a6b5d361daacee
Kitsunebackfire <kwolz92@gmail.com>
2023-02-10 08:51:55 -0600

    setup projects.js object, center contact h3 in contact.jsx

--- src/assets/projects/projects.js
+++ src/assets/projects/projects.js
@@ -1,3 +1,20 @@
-const projects = {
-  url: "https://boba-project-ki.herokuapp.com/",
-};
+const projects = [
+  {
+    name: "Boba Project",
+    url: "https://boba-project-ki.herokuapp.com/",
+    githubUrl: "https://github.com/Kitsunebackfire/boba_project",
+    description: "",
+  },
+  {
+    name: "Project Blog Client",
+    url: "https://kitsunebackfire.github.io/project-blog-client/",
+    githubUrl: "https://github.com/Kitsunebackfire/project-blog-client",
+    description: "",
+  },
+  {
+    name: "Project Blog Admin",
+    url: "https://kitsunebackfire.github.io/project-blog-admin/",
+    githubUrl: "https://github.com/Kitsunebackfire/project-blog-admin",
+    description: "",
+  },
+];
--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -7,7 +7,7 @@ const Contact = () => {
       className="relative h-full min-h-[500px] flex flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly items-center 
     mx-auto mt-[10%]"
     >
-      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl ">
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl ml-3">
         Contact
       </h3>
       <Mailer />
@@ -16,44 +16,3 @@ const Contact = () => {
 };
 
 export default Contact;
-
-/*
-<section className="contact__body">
-        <h1>
-          To <span className="underline">Reach Out</span> to Me
-        </h1>
-        <div className="contact__linkbody">
-          You can email me at{" "}
-          <a
-            className="underline contact__link"
-            href="mailto:kurtiveycodes@gmail.com"
-          >
-            kurtiveycodes@gmail.com
-          </a>
-        </div>
-        <div>or</div>
-        <div>
-          Message me on{" "}
-          <a
-            className="underline contact__link"
-            href="https://twitter.com/IveyCodingBJJ"
-          >
-            twitter
-          </a>
-          ,{" "}
-          <a
-            className="underline contact__link"
-            href="https://www.linkedin.com/in/k-ivey/"
-          >
-            linkedIn
-          </a>
-          , or{" "}
-          <a
-            className="underline contact__link"
-            href="https://www.instagram.com/iv.coding.bjj/?hl=en"
-          >
-            instagram
-          </a>
-        </div>
-      </section>
-*/
