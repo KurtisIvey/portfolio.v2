29ade83bea605d5e10c40b266a7511bc81e781ee
Kitsunebackfire <kwolz92@gmail.com>
2023-02-11 18:25:42 -0600

    create and configure photos.js

---
+++ src/assets/photos/photos.js
@@ -0,0 +1,41 @@
+import beltPromo from "./beltpromo.jpg";
+import kurtTrucking1 from "./kurtTrucking1.jpg";
+import kurtTrucking2 from "./kurtTrucking2.jpg";
+import kurtTrucking3 from "./kurtTrucking3.jpg";
+import kurtTrucking4 from "./kurtTrucking4.jpg";
+import tandem from "./tandembike.jpg";
+
+const photos = [
+  {
+    name: "my wife and I infront of my truck",
+    image: kurtTrucking1,
+    id: 11,
+  },
+  {
+    name: "me infront of my last truck before",
+    image: kurtTrucking2,
+    id: 12,
+  },
+  {
+    name: "my wife hanging off the side of my truck",
+    image: kurtTrucking3,
+    id: 13,
+  },
+  {
+    name: "solo view of my truck",
+    image: kurtTrucking4,
+    id: 14,
+  },
+  {
+    name: "promotion to purple belt",
+    image: beltPromo,
+    id: 15,
+  },
+  {
+    name: "my wife and I riding a tandem bike as she committed to pedaling the last stretch herself",
+    image: tandem,
+    id: 16,
+  },
+];
+
+export default photos;
