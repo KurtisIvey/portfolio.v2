7d7001e5d58afbd50af4a831b481b7624f46151a
Kitsunebackfire <kurtiveycodes@gmail.com>
2023-05-13 14:21:01 -0500

    update elements

--- src/components/Mailer.jsx
+++ src/components/Mailer.jsx
@@ -34,7 +34,7 @@ const Mailer = () => {
   }
   return (
     <div className="py-20  px-4 mx-auto max-w-screen-md mt-[30%] sm:mt-[25%] md:mt-[20%] lg:mt-[15%] xl:mt-[10%]">
-      <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 text-sm sm:text-xl xl:text-2xl">
+      <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 text-sm sm:text-lg xl:text-xl">
         Got a technical issue you need solved? Looking for a new employee to add
         to the team? Or if you just want to say hello. Feel free to reach out
         and I'll try my best to get back to you as soon as possible.
@@ -43,7 +43,7 @@ const Mailer = () => {
         <div>
           <label
             htmlFor="email"
-            className="block mb-2 text-base xl:text-xl font-medium text-gray-900 dark:text-gray-300"
+            className="block mb-2 text-base xl:text-lg font-medium text-gray-900 dark:text-gray-300"
           >
             Your email
           </label>
@@ -52,9 +52,8 @@ const Mailer = () => {
             id="email"
             name="user_email"
             onChange={handleChange}
-            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-base xl:text-xl rounded-lg
-               focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400
-               dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-base xl:text-lg rounded-lg
+               focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5"
             placeholder="yourEmail@gmail.com"
             required
           />
@@ -62,7 +61,7 @@ const Mailer = () => {
         <div>
           <label
             htmlFor="name"
-            className="block mb-2 text-base xl:text-xl sm:text-sm font-medium text-gray-900 dark:text-gray-300"
+            className="block mb-2 text-base xl:text-lg font-medium text-gray-900 dark:text-gray-300"
           >
             Your name
           </label>
@@ -71,8 +70,8 @@ const Mailer = () => {
             id="name"
             name="user_name"
             onChange={handleChange}
-            className="block p-3 w-full text-base xl:text-xl text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm 
-              focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+            className="block p-3 w-full text-base xl:text-lg text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm 
+              focus:ring-primary-500 focus:border-primary-500"
             placeholder="John Doe"
             required
           />
@@ -80,7 +79,7 @@ const Mailer = () => {
         <div className="sm:col-span-2">
           <label
             htmlFor="message"
-            className="block mb-2 text-base xl:text-xl font-medium text-gray-900 dark:text-gray-400"
+            className="block mb-2 text-base xl:text-lg font-medium text-gray-900 dark:text-gray-400"
           >
             Your message
           </label>
@@ -89,9 +88,9 @@ const Mailer = () => {
             name="message"
             onChange={handleChange}
             rows="6"
-            className="block p-2.5 w-full text-base xl:text-xl text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 
-              focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400
-               dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+            className="block p-2.5 w-full text-base xl:text-lg text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 
+              focus:ring-primary-500 focus:border-primary-500
+              "
             placeholder="Let me know how I can help you"
           />
         </div>
@@ -99,7 +98,7 @@ const Mailer = () => {
           type="submit"
           className="py-1.5 sm:py-3 px-3.5 sm:px-5 text-base xl:text-xl font-medium text-center bg-[#F04E35]/90 hover:bg-[#F04E35]/60 text-white 
           rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 
-          focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+          focus:outline-none focus:ring-primary-300 "
         >
           Send message
         </button>
--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -17,7 +17,7 @@ const About = () => {
 
       <div className=" grid grid-cols-1 lg:grid-cols-2 h-[71%]  mt-[30%] sm:mt-[25%]   gap-2 md:gap-4 ">
         <ImageCarousel />
-        <div className="text-xs sm:text-base flex flex-col justify-center mr-1 mt-[5%] lg:text-xl xxl:text-2xl ">
+        <div className="text-xs text-xs sm:text-base lg:text-lg xxl:text-xl flex flex-col justify-center mr-1 mt-[5%]">
           <p>
             Hi, my name is Kurtis and I enjoy creating websites from scratch. My
             interest in coding originally started while I was an over the road
@@ -37,7 +37,7 @@ const About = () => {
           </p>
           <br />
 
-          <p className="text-[.6rem] sm:text-sm lg:text-xl xxl:text-2xl">
+          <p className="text-[.6rem]  sm:text-base lg:text-lg xxl:text-xl">
             <span className=" text-[#F04E35] ">Fun Fact:</span> I've drove the
             distance equivalent to having driven around the world 28 times, so
             I'm pretty much a professional at sitting on my butt.
@@ -49,45 +49,3 @@ const About = () => {
 };
 
 export default About;
-/*<content>
-        <motion.div
-      initial={{ opacity: 0 }}
-      transition={{ duration: 1.5 }}
-      whileInView={{ opacity: 1 }}
-      viewport={{ once: true }}
-      className="relative h-screen flex flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly items-center mx-auto"
-    >
-      <h3 className="absolute sm:top-24 top-14  uppercase tracking-[20px] text-gray-500 text-2xl sm:ml-0 ml-6">
-        About
-      </h3>
-      <div className=" grid grid-cols-1 md:grid-cols-2 h-[69%] sm:h-[65%] mt-[1%] sm:mt-[12%] gap-2 sm:gap-10">
-        <ImageCarousel />
-        <div className="text-xs sm:text-base flex flex-col justify-center mr-1">
-          <p>
-            Hi, my name is Kurtis and I enjoy creating websites from scratch. My
-            interest in coding originally started while I was an over the road
-            truck driver/instructor for about 5 years. Unfortunately, trucking
-            is not conducive to a good marital life. Because of this, I sought
-            out an alternative career. Once my wife successfully immigrated from
-            Jamaica to the USA, I needed a game plan for the future and chose
-            coding.
-          </p>
-          <br />
-          <p>
-            Fast forward to the end of my trucking career, I decided to pursue
-            an education and eventually went down the path of a self taught
-            developer. I utilized resources such as Google, Youtube,
-            Freecodecamp, Udemy, docs,The Odin Project, etc... to further my
-            skills.
-          </p>
-          <br />
-
-          <p className="text-[.6rem] sm:text-sm">
-            <span className=" text-[#F04E35] ">Fun Fact:</span> I've drove the
-            distance equivalent to having driven around the world 28 times, so
-            I'm pretty much a professional at sitting on my butt.
-          </p>
-        </div>
-      </div>
-    </motion.div>
-      </content> */
